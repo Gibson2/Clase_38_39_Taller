7b4adb235f79c13741d92815019166cffa0e7e2c
Guillermo <gibsonguillermoarturo@gmail.com>
2017-06-20 01:37:41 -0400

    Actualizando el código

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def x
+
+  	@user = User.all
+
+  end
+
+  def new_user
+  	if params[:nombre].present? && params[:email].present? && params[:edad].present?
+  		@user = User.new(nombre: params[:nombre], email: params[:email],edad: params[:edad])
+  		@user.save
+  		edirect_to pages_x_path, notice: "Usuario Creado"
+  	else
+  		redirect_to pages_x_path, alert: "Ingresa todos los Datos"
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   root 'pages#x'
 
+  post 'pages/new_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
